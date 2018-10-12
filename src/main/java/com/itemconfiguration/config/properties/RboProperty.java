b97bd2dbc2359c00574263f63f9a2217f2fab02f
Oleksandr Kordunyan <kordunyanolexandr@gmail.com>
2018-10-12 19:03:50 +0300

    builded js

--- src/main/java/com/itemconfiguration/config/properties/RboProperty.java
+++ src/main/java/com/itemconfiguration/config/properties/RboProperty.java
@@ -1,9 +1,13 @@
 package com.itemconfiguration.config.properties;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RboProperty {
 	private String name;
 	private String code;
 	private String url;
+	private List<String> multifields = new ArrayList<>();
 
 	public String getName() {
 		return name;
@@ -27,5 +31,13 @@ public class RboProperty {
 
 	public void setUrl(String url) {
 		this.url = url;
+	}
+
+	public List<String> getMultifields() {
+		return multifields;
+	}
+
+	public void setMultifields(List<String> multifields) {
+		this.multifields = multifields;
 	}
 }
