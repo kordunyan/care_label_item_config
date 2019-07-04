b3fe07ae490e9f9d926d7cba09dbc88e9b3a7e80
Dmytro Kapustianskyi <dkapus@softserveinc.com>
2019-07-04 12:35:25 +0300

    Do not update schema by Hibernate. Removed redundant comments from InstructionTypeInputConfig.

--- src/main/java/com/itemconfiguration/domain/InstructionTypeInputConfig.java
+++ src/main/java/com/itemconfiguration/domain/InstructionTypeInputConfig.java
@@ -16,22 +16,9 @@ public class InstructionTypeInputConfig {
 			foreignKey = @ForeignKey(name = "instruction_type_input_config_instruction_type_name_fkey")
 	)
 	private DataType dataType;
-	private Boolean useFtp;
-	private String ftpServer;
-	private String ftpUser;
-	private String ftpPassword;
-	private String ftpFolder;
-	private String fileNamePattern;
-	private String workFolder;
-	private String sheetName;
-	private Integer sheetIdx;
 	private String insertType;
 	private String codeColumnIdx;
 	private String codeColumnName;
-	private Integer expectedColumnsQty;
-	private Integer expectedSheetsQty;
-	private Integer headerIdx;
-	private Integer dataRowIdx;
 	private String deleteCodeColumnIdx;
 
 	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
@@ -55,78 +42,6 @@ public class InstructionTypeInputConfig {
 
 	public void setDataType(DataType dataType) {
 		this.dataType = dataType;
-	}
-
-	public Boolean getUseFtp() {
-		return useFtp;
-	}
-
-	public void setUseFtp(Boolean useFtp) {
-		this.useFtp = useFtp;
-	}
-
-	public String getFtpServer() {
-		return ftpServer;
-	}
-
-	public void setFtpServer(String ftpServer) {
-		this.ftpServer = ftpServer;
-	}
-
-	public String getFtpUser() {
-		return ftpUser;
-	}
-
-	public void setFtpUser(String ftpUser) {
-		this.ftpUser = ftpUser;
-	}
-
-	public String getFtpPassword() {
-		return ftpPassword;
-	}
-
-	public void setFtpPassword(String ftpPassword) {
-		this.ftpPassword = ftpPassword;
-	}
-
-	public String getFtpFolder() {
-		return ftpFolder;
-	}
-
-	public void setFtpFolder(String ftpFolder) {
-		this.ftpFolder = ftpFolder;
-	}
-
-	public String getFileNamePattern() {
-		return fileNamePattern;
-	}
-
-	public void setFileNamePattern(String fileNamePattern) {
-		this.fileNamePattern = fileNamePattern;
-	}
-
-	public String getWorkFolder() {
-		return workFolder;
-	}
-
-	public void setWorkFolder(String workFolder) {
-		this.workFolder = workFolder;
-	}
-
-	public String getSheetName() {
-		return sheetName;
-	}
-
-	public void setSheetName(String sheetName) {
-		this.sheetName = sheetName;
-	}
-
-	public Integer getSheetIdx() {
-		return sheetIdx;
-	}
-
-	public void setSheetIdx(Integer sheetIdx) {
-		this.sheetIdx = sheetIdx;
 	}
 
 	public String getInsertType() {
@@ -153,38 +68,6 @@ public class InstructionTypeInputConfig {
 		this.codeColumnName = codeColumnName;
 	}
 
-	public Integer getExpectedColumnsQty() {
-		return expectedColumnsQty;
-	}
-
-	public void setExpectedColumnsQty(Integer expectedColumnsQty) {
-		this.expectedColumnsQty = expectedColumnsQty;
-	}
-
-	public Integer getExpectedSheetsQty() {
-		return expectedSheetsQty;
-	}
-
-	public void setExpectedSheetsQty(Integer expectedSheetsQty) {
-		this.expectedSheetsQty = expectedSheetsQty;
-	}
-
-	public Integer getHeaderIdx() {
-		return headerIdx;
-	}
-
-	public void setHeaderIdx(Integer headerIdx) {
-		this.headerIdx = headerIdx;
-	}
-
-	public Integer getDataRowIdx() {
-		return dataRowIdx;
-	}
-
-	public void setDataRowIdx(Integer dataRowIdx) {
-		this.dataRowIdx = dataRowIdx;
-	}
-
 	public String getDeleteCodeColumnIdx() {
 		return deleteCodeColumnIdx;
 	}
@@ -200,4 +83,5 @@ public class InstructionTypeInputConfig {
 	public void setInstructionTypeLanguageInputPositions(List<InstructionTypeLanguageInputPosition> instructionTypeLanguageInputPositions) {
 		this.instructionTypeLanguageInputPositions = instructionTypeLanguageInputPositions;
 	}
+
 }
